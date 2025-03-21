4789428bf79b6141b476b43f0b772cec9c6cfde8
Sujay <s1dewalker23@gmail.com>
2025-03-21 18:50:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -178,9 +178,10 @@ if st.session_state.pairs:
         
         # Show chart in Streamlit
         st.plotly_chart(fig2)
+        st.divider()
         
     except Exception as e:
         st.error(f"🚨 Error analysing price ratio data: {e}")
     
 
-st.divider()
+

d850cb5c8366cdfff7ab7d7d29abac663c0ee630
Sujay <s1dewalker23@gmail.com>
2025-03-24 17:02:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import yfinance as yf
 from datetime import datetime, timedelta
 import plotly.express as px

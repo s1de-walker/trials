470c45c05ffbde01380a8a0fa35fca7dc2cb6025
Sujay <s1dewalker23@gmail.com>
2025-03-24 12:52:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -151,6 +151,19 @@ if st.session_state.pairs:
         lower_percentile = price_ratio_df['Price ratio'].quantile(percentile / 100)
         upper_percentile = price_ratio_df['Price ratio'].quantile(1 - percentile / 100)
 
+        fig3 = go.Figure()
+
+        # Add the main line with dynamic color
+        colors = ["black" if (val < lower_percentile or val > upper_percentile) else "#E7E6E6" for val in price_ratio_df['Price ratio']]
+
+        fig.add_trace(go.Scatter(
+            x = price_ratio_df['Date'], y = price_ratio_df["Price ratio"],
+            mode = 'lines', line = dict(width = 2), marker = dict(color = colors), name = "Price ratio"
+        ))
+
+        # Add horizontal lines
+        
+
         # Create Plotly figure
         fig2 = px.line(
             price_ratio_df,

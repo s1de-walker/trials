0f2ed2e5f8f320a566de557d08502b33a2b54a8d
Sujay <s1dewalker23@gmail.com>
2025-03-26 15:38:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -406,8 +406,9 @@ if st.session_state.pairs:
 
 
         st.write(" ")
+        st.subheader("Cointegration Residuals)
         st.markdown("""
-        ##### <span style="color:#A55B4B">Cointegration residuals</span> = Deviations of actual values from predicted relationship
+        ###### <span style="color:#A55B4B">Cointegration residuals</span> = Deviations of actual values from predicted relationship
         """, unsafe_allow_html=True)
         #-------------------------------------------------------------------------
         st.caption("*How far the spread is from its **\"fair value\"***")

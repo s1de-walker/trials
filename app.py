7398f1317d3c50817d2cb227670b94830fab4e00
Sujay <s1dewalker23@gmail.com>
2025-03-26 16:05:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -331,13 +331,16 @@ if st.session_state.pairs:
         fig_rb = px.line(rolling_beta, title=f"Rolling Beta of {ticker1} on {ticker2} ({window}-day window)", color_discrete_sequence=['#A55B4B'])
         fig_rb.update_layout(showlegend=False)  # Remove the legend
 
-        # Add horizontal line at y = 1
+        # Calculate the mean of the rolling beta
+        mean_rolling_beta = rolling_beta.mean()
+        
+        # Add horizontal line at the mean of the rolling beta
         fig_rb.add_shape(
             type="line",
             x0=rolling_beta.index.min(),
             x1=rolling_beta.index.max(),
-            y0=1,
-            y1=1,
+            y0=mean_rolling_beta,
+            y1=mean_rolling_beta,
             line=dict(color="white", width=2, dash="solid")
         )
 
